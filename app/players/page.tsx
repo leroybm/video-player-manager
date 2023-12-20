806ac8cfeee1d0255ad3419daebc86137298a555
Bernardo Trindade de Abreu <btrindadedeabreu@btrindadedeabreu-j26ym-ustwo.home>
2023-12-20 11:16:22 +0000

    feat: integrate players module changes to the new sidebar

--- app/players/page.tsx
+++ app/players/page.tsx
@@ -1,10 +1,71 @@
-export default function PlayersPage() {
-  return (
-    <>
-      <span className="font-bold text-4xl">Players</span>
+'use client';
 
-      <div className="border-dashed border border-zinc-500 w-full h-12 rounded-lg"></div>
-      <div className="border-dashed border border-zinc-500 w-full h-64 rounded-lg"></div>
-    </>
-  )
+import Link from 'next/link';
+import { useEffect } from 'react';
+import { usePlayerService } from '../../services';
+
+export default function Players() {
+    const playerService = usePlayerService();
+    const players = playerService.players;
+
+    useEffect(() => {
+        playerService.getAll();
+    }, []);
+
+    return (
+        <>
+            <h1>Video Players Pass</h1>
+            <Link href="/players/add">Create New</Link>
+            <table>
+                <thead>
+                    <tr>
+                        <th style={{ width: '30%' }}>Title</th>
+                        <th style={{ width: '10%' }}></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <TableBody />
+                </tbody>
+            </table>
+        </>
+    );
+
+    function TableBody() {
+        if (players?.length) {
+            return (players.map(player =>
+                <tr key={player.id}>
+                    <td>{player.title}</td>
+                    <td style={{ whiteSpace: 'nowrap' }}>
+                        <Link href={`/players/edit/${player.id}`}>Edit</Link>
+                        <button onClick={() => playerService.delete(player.id)} style={{ width: '60px' }} disabled={player.isDeleting}>
+                            {player.isDeleting
+                                ? <span>Spinner</span>
+                                : <span>Delete</span>
+                            }
+                        </button>
+                    </td>
+                </tr>
+            ));
+        }
+
+        if (!players) {
+            return (
+                <tr>
+                    <td colSpan={4}>
+                        <span>Spinner</span>
+                    </td>
+                </tr>
+            );
+        }
+
+        if (players?.length === 0) {
+            return (
+                <tr>
+                    <td colSpan={4}>
+                        <div>No Players To Display</div>
+                    </td>
+                </tr>
+            );
+        }
+    }
 }
