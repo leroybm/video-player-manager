55601ac51ce727acdc115c04f902b95aae92610d
romansharapov19 <roman.s.19@hotmail.com>
2024-01-08 15:06:01 +0000

    feat(9): fix deploy error

--- app/api/players/count/route.ts
+++ app/api/players/count/route.ts
@@ -1,9 +1,11 @@
 import { playersRepo } from "@/lib/server"
 import { apiHandler } from "@/lib/server/api"
 
-module.exports = apiHandler({
+const handlers = apiHandler({
   GET: getPalayersCount,
 })
+
+export const { GET } = handlers
 
 async function getPalayersCount() {
   return await playersRepo.count()
