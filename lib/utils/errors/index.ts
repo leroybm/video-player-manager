decb362e41473a6d2d42a245d80a9036ca150d0b
Leroy Medeiros <leroy.medeiros@ustwo.com>
2024-01-18 15:42:07 +0000

    refactor: applies PR comments suggestions

--- lib/utils/errors/index.ts
+++ lib/utils/errors/index.ts
@@ -1,14 +1,7 @@
+import { isObject } from "../type-validation"
+
 interface ErrorLike {
   message: string
-}
-
-/**
- * A simple type guard for objects.
- *
- * @param obj - A possible object
- */
-export const isObject = (obj: unknown): obj is Record<string, unknown> => {
-  return typeof obj === "object" && obj !== null
 }
 
 /**
