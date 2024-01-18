10bb6e4b7e1570af60cebe8747a3cd9a5d9c5d91
Leroy Medeiros <leroy.medeiros@ustwo.com>
2024-01-18 15:51:28 +0000

    test: moves type-validation tests

--- lib/utils/errors/index.spec.ts
+++ lib/utils/errors/index.spec.ts
@@ -1,32 +1,4 @@
-import { getErrorMessage, isErrorLike, isObject } from "."
-
-const ARRAY: unknown[] = []
-const BOOLEAN = true
-const FUNCTION = () => {}
-const NULL = null
-const NUMBER = 0
-const OBJECT = {}
-const STRING = ""
-const UNDEFINED = undefined
-
-class CLASS {} // `CLASS` is a function and `new CLASS()` is an Object
-
-test("isObject returns true for objects only", () => {
-  for (const item of [ARRAY, new CLASS(), OBJECT]) {
-    expect(isObject(item)).toBe(true)
-  }
-  for (const item of [
-    BOOLEAN,
-    CLASS,
-    FUNCTION,
-    NULL,
-    NUMBER,
-    STRING,
-    UNDEFINED,
-  ]) {
-    expect(isObject(item)).toBe(false)
-  }
-})
+import { getErrorMessage, isErrorLike } from "."
 
 test("isErrorLike returns true when object is like an error", () => {
   expect(isErrorLike(new Error())).toBe(true)
